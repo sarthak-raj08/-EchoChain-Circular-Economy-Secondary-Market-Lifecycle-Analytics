5632d8b9d438013ef132fd372290a4b14c349d6d
ARYANSHINGALA <aryanshingala1234@gmial.com>
2026-08-26 20:39:53 +0530

    working on scrpy part

--- scrapy/echomarket/spiders/marketplace_spider.py
+++ scrapy/echomarket/spiders/marketplace_spider.py
@@ -1,12 +1,45 @@
+# import scrapy
+
+
+# class MarketplaceSpider(scrapy.Spider):
+
+#     name = "marketplace"
+
+#     start_urls = [
+#         "TARGET_WEBSITE_URL"
+#     ]
+
+#     def parse(self, response):
+
+#         for product in response.css("PRODUCT_SELECTOR"):
+
+#             yield {
+#                 "Product_Name": product.css("NAME_SELECTOR::text").get(),
+#                 "Brand": product.css("BRAND_SELECTOR::text").get(),
+#                 "Price_INR": product.css("PRICE_SELECTOR::text").get(),
+#                 "Condition": product.css("CONDITION_SELECTOR::text").get(),
+#                 "Location": product.css("LOCATION_SELECTOR::text").get(),
+#             }
+
 import scrapy
 
 
 class MarketplaceSpider(scrapy.Spider):
+
     name = "marketplace"
 
     start_urls = [
-        "https://example.com"
+        "https://books.toscrape.com/"
     ]
 
     def parse(self, response):
-        self.logger.info("Spider is working!")
+
+        for book in response.css("article.product_pod"):
+
+            yield {
+                "title": book.css("h3 a::attr(title)").get(),
+                "price": book.css("p.price_color::text").get(),
+                "availability": book.css("p.instock.availability::text").getall(),
+                "product_url": book.css("h3 a::attr(href)").get(),
+            }
+
